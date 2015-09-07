68e5eb9dd75625261bd8ab5af5ed8b9ff528dc58
laoyoutiao <yuwenhui@yuwenhuideiMac.local>
2015-09-07 16:46:45 +0800

    2015/9/7

---
+++ ChangeCritical/MyHeader.h
@@ -0,0 +1,26 @@
+//
+//  MyHeader.h
+//  ChangeCritical
+//
+//  Created by 玉文辉 on 15/9/7.
+//  Copyright (c) 2015年 yuwenhui. All rights reserved.
+//
+
+#ifndef ChangeCritical_MyHeader_h
+#define ChangeCritical_MyHeader_h
+
+#define ScreenSize [UIScreen mainScreen].bounds.size
+
+#define NSLogStr(x) NSLog(@"%@",x);
+
+#define NSLogInt(x) NSLog(@"%d",x);
+
+#define NSLogInteger(x) NSLog(@"%ld",x);
+
+#define NSLogFloat(x) NSLog(@"%f",x);
+
+#define NSLogCGFloat(x) NSLog(@"%lf",x);
+
+#define NSLogBool(x) NSLog(@"%u",x);
+
+#endif
